686e77f1894dc559447d545576e73dd7b2a02bb1
Dru Lang <drulang@gmail.com>
2016-02-28 15:51:50 -0500

    Add empty ios project

---
+++ GrapeVine/GrapeVine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrapeVine
+//
+//  Created by Dru Lang on 2/28/16.
+//  Copyright © 2016 Dru Lang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
